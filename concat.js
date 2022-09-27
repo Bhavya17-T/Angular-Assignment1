3804cbe10304eaf672807882ac62a4020cddd558
Bhavya17-T <109224832+Bhavya17-T@users.noreply.github.com>
2022-09-27 21:18:43 +0530

    Add files via upload

--- concat.js
+++ concat.js
@@ -1,11 +1,10 @@
-/** PURE_IMPORTS_START _of,_operators_concatAll PURE_IMPORTS_END */
-import { of } from './of';
-import { concatAll } from '../operators/concatAll';
+/** PURE_IMPORTS_START _observable_concat PURE_IMPORTS_END */
+import { concat as concatStatic } from '../observable/concat';
 export function concat() {
     var observables = [];
     for (var _i = 0; _i < arguments.length; _i++) {
         observables[_i] = arguments[_i];
     }
-    return concatAll()(of.apply(void 0, observables));
+    return function (source) { return source.lift.call(concatStatic.apply(void 0, [source].concat(observables))); };
 }
 //# sourceMappingURL=concat.js.map
